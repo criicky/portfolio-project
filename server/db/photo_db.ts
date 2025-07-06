d1d196c97a2c9ac06963ec8ce0f148ae11bc84cc
criicky <scan.riccardo@gmail.com>
2025-07-06 17:42:31 +0200

    Add initial project structure with server setup and database integration

---
+++ server/db/photo_db.ts
@@ -0,0 +1,9 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('db.db', (err) =>{
+    if (err) {
+        console.error('Error opening database:', err.message);
+    } else {
+        console.log('Connected to the database.');
+    }
+});
